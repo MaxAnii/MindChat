5bbddc3725b6d58b67f6ebdebc86f8c2fa61578c
MaxAnii <ansarbaba2000@gmail.com>
2025-12-28 14:13:34 +0530

    Chore: Config dotenv

--- chatMind-be/lib/prisma.ts
+++ chatMind-be/lib/prisma.ts
@@ -1,8 +1,16 @@
+import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+dotenv.config();
+
+const databaseUrl = process.env.DATABASE_URL;
+if (!databaseUrl) {
+	throw new Error("DATABASE_URL is not set");
+}
+
+const pool = new Pool({ connectionString: databaseUrl });
 const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({ adapter });

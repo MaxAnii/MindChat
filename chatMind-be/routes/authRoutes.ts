e7d79a4d2dc701f3ff7754f12fbf7b33d5dabfc4
MaxAnii <ansarbaba2000@gmail.com>
2025-12-28 21:54:45 +0530

    Refactor: Get user image url.

--- chatMind-be/routes/authRoutes.ts
+++ chatMind-be/routes/authRoutes.ts
@@ -228,6 +228,7 @@ router.get("/me", authMiddleware, async (req: any, res: Response) => {
 				name: true,
 				about: true,
 				isVerified: true,
+				imageURL: true,
 			},
 		});
 		if (!user) return res.status(404).json({ message: "User not found" });

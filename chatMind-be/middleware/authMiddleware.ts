034074049613639ebd24a51b56c37c2a6a25070e
MaxAnii <ansarbaba2000@gmail.com>
2025-12-28 21:58:29 +0530

    Chore: Set email user in the req header.

--- chatMind-be/middleware/authMiddleware.ts
+++ chatMind-be/middleware/authMiddleware.ts
@@ -20,6 +20,7 @@ const authMiddleware = (
 			userId: string;
 		};
 		req.userId = decoded.userId;
+		req.userEmail = decoded.userEmail;
 		next();
 	} catch (err) {
 		return res.status(401).json({ message: "Invalid token" });

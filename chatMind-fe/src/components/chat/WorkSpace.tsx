5e32bb2f6c795db10886f6fb032ec71be4de8931
MaxAnii <ansarbaba2000@gmail.com>
2025-12-29 12:52:16 +0530

    Feat: Creates logout component.

--- chatMind-fe/src/components/chat/WorkSpace.tsx
+++ chatMind-fe/src/components/chat/WorkSpace.tsx
@@ -1,6 +1,5 @@
-import { LogOut } from "lucide-react";
-import { Button } from "../ui/button";
 import UserProfileModal from "../userProfile/userProfileModal";
+import Logout from "../auth/logout";
 
 const Workspace = () => {
 	return (
@@ -9,9 +8,7 @@ const Workspace = () => {
 				<UserProfileModal />
 			</div>
 			<div className="h-10 w-10 rounded-xl bg-muted flex items-center justify-center text-sm">
-				<Button variant="default" size="icon">
-					<LogOut className="h-4 w-4" />
-				</Button>
+				<Logout />
 			</div>
 		</>
 	);
